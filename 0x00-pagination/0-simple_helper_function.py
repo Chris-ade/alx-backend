5f3b053bfdcd1accaf3feb26a163896885132350
Chris Ade <101924502+Chris-ade@users.noreply.github.com>
2023-02-20 08:19:55 +0100

    Update 0-simple_helper_function.py

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,14 +1,10 @@
 #!/usr/bin/env python3
-""" 
-Helper function module
-"""
-from typing import Tuple
+""" Helper function module """
 
 
-def index_range(page: int, page_size: int) -> Tuple[int, int]:
+def index_range(page: int, page_size: int) -> tuple:
     """
-    Return a tuple of start and end indexes
-    for a given page and page size.
+    Return a tuple of start and end indexes for a given page and page size.
     """
     start = (page - 1) * page_size
     end = start + page_size
