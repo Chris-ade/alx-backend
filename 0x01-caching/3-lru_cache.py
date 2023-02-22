5da69b7c2b32fe34dff6dbda316c3893ee2403c9
Chris Ade <101924502+Chris-ade@users.noreply.github.com>
2023-02-22 22:19:46 +0100

    Task done

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,46 +1,47 @@
 #!/usr/bin/python3
-"""
-LRU caching system
-"""
-
-BaseCaching = __import__('base_caching').BaseCaching
+""" LRU Caching """
+from base_caching import BaseCaching
 
 
 class LRUCache(BaseCaching):
-    """
-    LRU cache system that inherits from BaseCaching
-    """
-
+    """ Class that inherits from BaseCaching and is a caching system """
     def __init__(self):
-        """
-        Initializes the LRU Cache
-        """
         super().__init__()
-        self.order = []
+        self.head, self.tail = '-', '='
+        self.next, self.prev = {}, {}
+        self.handle(self.head, self.tail)
+
+    def handle(self, head, tail):
+        """ LRU algorithm, handle elements """
+        self.next[head], self.prev[tail] = tail, head
+
+    def _remove(self, key):
+        """ LRU algorithm, remove element """
+        self.handle(self.prev[key], self.next[key])
+        del self.prev[key], self.next[key], self.cache_data[key]
+
+    def _add(self, key, item):
+        """ LRU algorithm, add element """
+        self.cache_data[key] = item
+        self.handle(self.prev[self.tail], key)
+        self.handle(key, self.tail)
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            print("DISCARD: {}".format(self.next[self.head]))
+            self._remove(self.next[self.head])
 
     def put(self, key, item):
-        """
-        Adds an item to the cache
-        """
-        if key is not None and item is not None:
-            if len(self.cache_data) >= self.MAX_ITEMS:
-                if key in self.cache_data:
-                    self.order.remove(key)
-                    del self.cache_data[key]
-                else:
-                    del self.cache_data[self.order.pop(0)]
-                print("DISCARD:", self.order[0])
-            self.cache_data[key] = item
-            if key in self.order:
-                self.order.remove(key)
-            self.order.append(key)
+        """ Assign to the dictionary """
+        if key and item:
+            if key in self.cache_data:
+                self._remove(key)
+            self._add(key, item)
 
     def get(self, key):
-        """
-        Retrieves an item from cache
-        """
-        if key is None or key not in self.cache_data:
+        """ Return the value linked """
+        if key is None or self.cache_data.get(key) is None:
             return None
-        self.order.remove(key)
-        self.order.append(key)
-        return self.cache_data[key]
+        if key in self.cache_data:
+            value = self.cache_data[key]
+            self._remove(key)
+            self._add(key, value)
+            return value
